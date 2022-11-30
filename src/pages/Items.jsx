51221e6c2627dbaddb3f900a7916703fac22133a
Daniel-N98 <Danieldnee@hotmail.com>
2022-11-30 21:22:52 +0000

    Refactored itemlist to use cached items

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -6,14 +6,11 @@ import { fetchItems } from "../utils/requests";
 
 export default function Items() {
   const [searchTerm, setSearchTerm] = useState("");
-  const itemsIn = fetchItems().then((items) => {
-    return items;
-  });
   return (
     <section className="items-list">
       <ItemSearch searchTermState={{ searchTerm, setSearchTerm }} />
       <h1 className="text-center">Items page</h1>
-      <ItemsList searchTerm={searchTerm} itemsIn={itemsIn} />
+      <ItemsList searchTerm={searchTerm} />
     </section>
   );
 }
